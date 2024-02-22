b58bf3945f61c78e5b7af293bc98a945ccdbadea
jk <yjkeong@gmail.com>
2024-02-22 19:06:05 +0900

    ✨ feat : Mainページ作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import './App.css'
+import { Route, Routes } from 'react-router-dom'
+import MainPage from './pages/MainPage'
 
 function App() {
-
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <Routes>
+      <Route path='/' element={<MainPage/>} />
+    </Routes>
   )
 }
 
