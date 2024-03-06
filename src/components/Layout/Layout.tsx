d0746e411b454a1a8d4a1ff49f28d3ab2e7c7d27
jk <yjkeong@gmail.com>
2024-03-06 14:03:11 +0900

    ✨ feat : グローバルレイアウトにバナー追加

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,12 +1,22 @@
 import Navbar from './Header/Navbar'
 import Body from './Body/Body'
 import Footer from './Footer/Footer'
+import Banner from './Header/Banner'
+import { useLocation } from 'react-router-dom';
+
+const pageTitles: { [key: string]: string[] } = {
+  "/board": ["파충류 이모저모", "주인님 같이 놀아요!"],
+}
 
 const Layout = () => {
+  const location = useLocation();
+  const titles = pageTitles[location.pathname];
+
   return (
     <>
       <Navbar />
-      <Body/>
+      {titles && <Banner titles={titles} />}
+      <Body />
       <Footer />
     </>
   )
