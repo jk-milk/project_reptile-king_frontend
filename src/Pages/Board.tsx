c2036da68b4f63b31f06f8d862e91821c6a4556b
jk <yjkeong@gmail.com>
2024-03-06 20:05:10 +0900

    🔨 chore : ドロップダウンの実装方法の変更

--- src/Pages/Board.tsx
+++ src/Pages/Board.tsx
@@ -13,7 +13,8 @@ function Board() {
   const [selectedSubCategory, setSelectedSubCategory] = useState<string>("전체 게시글"); // 선택된 세부 카테고리
   const [filter, setFilter] = useState('전체글'); // 전체글, 인기글 필터
   const [sort, setSort] = useState('등록순'); // 정렬 방식
-  
+  const [dropdownOpen, setDropdownOpen] = useState(false);
+
   const LIKES_NUMBER = 5; // 인기글 조건 좋아요 수
 
   useEffect(() => {
@@ -58,32 +59,34 @@ function Board() {
     <div className="w-body m-auto">
       <div className="flex">
         <div className="w-44 mr-20">
-          Search
-          {categories.map((category) => (
-            <div key={category.id} className="w-full p-2">
-              <div className="border-b-2 pb-2">
-                <p className="text-lg text-white ps-2">
-                  {category.category}
-                </p>
+          search {/* 검색 구현 */}
+          <div className="mt-28">
+            {categories.map((category) => (
+              <div key={category.id} className="w-full p-2">
+                <div className="border-b-2 pb-2">
+                  <p className="text-lg text-white ps-2">
+                    {category.category}
+                  </p>
+                </div>
+                <ul className="w-full">
+                  {category.subCategories.map((subCategory) => (
+                    <li key={subCategory.title}>
+                      <button
+                        className={
+                          selectedSubCategory === subCategory.title
+                            ? "ps-2 py-0.5 text-white font-bold hover:underline"
+                            : "ps-2 py-0.5 text-white hover:font-bold hover:underline"
+                        }
+                        onClick={() => setSelectedSubCategory(subCategory.title)}
+                      >
+                        {subCategory.title}
+                      </button>
+                    </li>
+                  ))}
+                </ul>
               </div>
-              <ul className="w-full">
-                {category.subCategories.map((subCategory) => (
-                  <li key={subCategory.title}>
-                    <button
-                      className={
-                        selectedSubCategory === subCategory.title
-                          ? "ps-2 py-0.5 text-white font-bold hover:underline"
-                          : "ps-2 py-0.5 text-white hover:font-bold hover:underline"
-                      }
-                      onClick={() => setSelectedSubCategory(subCategory.title)}
-                    >
-                      {subCategory.title}
-                    </button>
-                  </li>
-                ))}
-              </ul>
-            </div>
-          ))}
+            ))}
+          </div>
         </div>
         <div className="w-[59.75rem]"> {/* 크기 조정 후 tailwind.config.js에 추가할 것 */}
           <h1 className="text-white text-2xl mt-5 pb-5">
@@ -91,29 +94,44 @@ function Board() {
           </h1>
           <div className="pb-3 flex justify-between">
             <div>
-              <button className="text-gray-900 bg-white border border-gray-800 hover:bg-gray-100 focus:ring-1 focus:ring-gray-800 font-bold text-sm pl-1.5 pr-2 py-1 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
+              <button className="text-gray-900 border border-gray-100 rounded-full focus:outline-none bg-white hover:bg-gray-100 focus:ring-1 focus:ring-gray-300 font-bold text-sm mr-2 pl-2 pr-2 py-1.5"
                 onClick={() => setFilter('전체글')}
               >
                 <RiFileList2Line className="inline-block pe-1 pb-1" />
                 전체글
               </button>
-              <button className="focus:outline-none text-white bg-red-700 hover:bg-red-800 border border-red-700 focus:ring-1 focus:ring-red-300 font-bold text-sm pl-1.5 pr-2 py-1 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"
+              <button className="text-white border border-red-700 rounded-full focus:outline-none bg-red-700 hover:bg-red-800 focus:ring-1 focus:ring-red-700 font-bold text-sm mr-2 pl-2 pr-2 py-1.5"
                 onClick={() => setFilter('인기글')}
               >
                 <HiMiniFire className="inline-block pe-1 pb-1" />
                 인기글
               </button>
-              <select
-                className="border border-gray-800 font-bold text-sm pl-1.5 pr-2 py-1"
-                value={sort}
-                onChange={(e) => setSort(e.target.value)}
+              <button
+                onClick={() => setDropdownOpen(!dropdownOpen)}
+                className="text-gray-900 border border-gray-100 bg-white hover:bg-gray-100 focus:ring-1 focus:outline-none focus:ring-gray-300 font-bold text-sm pl-2 pr-2 py-1.5 rounded-full inline-flex items-center"
               >
-                <option value="등록순">등록순</option>
-                <option value="최신순">최신순</option>
-                {/* <option value="댓글순">댓글순</option> */}
-              </select>
+                {sort}
+                <svg className="w-2.5 h-2.5 ms-1 mt-0.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+                  <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4" />
+                </svg>
+              </button>
+              {dropdownOpen &&
+                <div className="absolute z-10 bg-white divide-y divide-gray-100 rounded-lg shadow mt-1 ml-40 w-44">
+                  <ul className="py-2 text-sm text-gray-700">
+                    <li>
+                      <button onClick={() => { setSort('등록순'); setDropdownOpen(false); }} className="block px-4 py-2 w-full text-left hover:bg-gray-100">등록순</button>
+                    </li>
+                    <li>
+                      <button onClick={() => { setSort('최신순'); setDropdownOpen(false); }} className="block px-4 py-2 w-full text-left hover:bg-gray-100">최신순</button>
+                    </li>
+                    {/* <li>
+              <button onClick={() => { setSort('댓글순'); setDropdownOpen(false); }} className="block px-4 py-2 hover:bg-gray-100">댓글순</button>
+            </li> */}
+                  </ul>
+                </div>
+              }
             </div>
-            <button className="text-gray-900 bg-white border border-gray-800 hover:bg-gray-100 focus:ring-1 focus:ring-gray-800 font-bold text-sm pl-1.5 pr-2 py-1 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700">
+            <button className="text-gray-900 border border-gray-100 rounded-full focus:outline-none bg-white  hover:bg-gray-100 focus:ring-1 focus:ring-gray-300 font-bold text-sm pl-2 pr-2 py-1.5">
               <RiFileList2Line className="inline-block pe-1 pb-1" />
               글쓰기
             </button>
@@ -135,7 +153,7 @@ function Board() {
                     key={post.id}
                     className="p-3 text-sm"
                   >
-                    <Link to={`/board/${post.id}`}>{post.title}</Link>
+                    <Link to={`/board/${post.id}`}>{post.title}</Link> {/* 두 줄 고정으로 할 것 */}
                   </div>
                 ))}
             </div>
