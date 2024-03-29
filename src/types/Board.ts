a45df52af1c0a54511d1570b8b9e9b0b8a94c6f0
jk <yjkeong@gmail.com>
2024-03-29 16:11:28 +0900

    🔨 chore : DB修正によるタイプ修正

--- src/types/Board.ts
+++ src/types/Board.ts
@@ -15,7 +15,8 @@ export type PostCategory = {
   id: number;
   category: string;
   subCategories: {
-    title: string
+    title: string;
+    link: string;
   }[];
 }
 
@@ -25,9 +26,9 @@ export type Comment = {
   post_id: number;
   content: string;
   group_comment_id: number;
-  parent_comment_id?: number,
-  depth_no: number,
-  order_no: number,
+  parent_comment_id?: number;
+  depth_no: number;
+  order_no: number;
   created_at: string;
   updated_at: string;
 }
