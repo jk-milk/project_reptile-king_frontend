6042b5c2e2d51ce656c1aacc6c60ee26840ce780
jk <yjkeong@gmail.com>
2024-04-03 16:31:39 +0900

    🔨 chore : quill editorを使用して投稿作成ページ実装

--- src/Pages/PostWrite.tsx
+++ src/Pages/PostWrite.tsx
@@ -1,36 +1,83 @@
-import { useEffect, useState } from "react";
-import BoardCategory from "../components/Board/BoardCategory";
-import { Post, PostCategory } from "../types/Board";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+// import { API } from "../config";
 import axios from "axios";
+import BoardCategory from "../components/Board/BoardCategory";
 import PopularPosts from "../components/Board/PopularPosts";
+import QuillEditor from "../components/Board/QuillEditor";
+import CategoryWriteDropdown from "../components/Board/CategoryWriteDropdown";
 
 function PostWrite() {
-  
-  const [categories, setCategories] = useState<PostCategory[]>([]);
-  const [posts, setPosts] = useState<Post[]>([]);
-  const [selectedSubCategory, setSelectedSubCategory] = useState<string>("전체 게시글"); // 선택된 세부 카테고리
+  const navigate = useNavigate();
+  const [dropdownOpen, setDropdownOpen] = useState(false);
+  const [subCategory, setSubCategory] = useState("카테고리 선택");
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
 
-  useEffect(() => {
-    const fetchPosts = async () => {
-      // const categoriesResponse = await axios.get('http://localhost:8080/categories'); // 실제 api
-      // const postsResponse = await axios.get('http://localhost:8080/api/posts');
-      const categoriesResponse = await axios.get('http://localhost:3300/categories'); // json server
-      const postsResponse = await axios.get('http://localhost:3300/posts');
-
-      setCategories(categoriesResponse.data);
-      setPosts(postsResponse.data);
+  const handleSubmit = async () => {
+    if (subCategory === "카테고리 선택") {
+      alert("카테고리를 선택해 주세요!")
+      return;
+    } else if (!title) {
+      alert("제목을 입력해 주세요!")
+      return;
+    }
+      
+    const postData = {
+      category: subCategory,
+      title: title,
+      content: content,
     };
-    fetchPosts();
-  });
 
+    try {
+      // const response = await axios.post(API+"posts", postData); // API
+      const response = await axios.post('http://localhost:3300/posts', postData); // JSON server
+      console.log(response.data);
+      alert("글 작성 완료!");
+      navigate(`/board/category/${subCategory}`) // /board/category/:link
+    } catch (error) {
+      console.error('서버 오류', error);
+      alert("서버 오류! 다시 등록해 주세요.");
+    }
+  };
 
-  return(
+  return (
     <div className="laptop:w-[75rem] w-body m-auto flex">
-      <BoardCategory categories={categories} selectedSubCategory={selectedSubCategory} setSelectedSubCategory={setSelectedSubCategory} />
+      <BoardCategory />
       <div className="laptop:w-[47.6875rem] w-mainContent">
-        ㅇ
+        <h1 className="text-white text-2xl mt-5 pb-5">
+          글쓰기
+        </h1>
+        <div className="mb-4 p-2 min-h-[35rem] bg-gray-200 rounded">
+          <button className="m-2 p-2 w-44 border border-gray-400 text-gray-900 bg-white focus:ring-2 focus:outline-none focus:ring-gray-300 font-semibold rounded inline-flex items-center justify-between" onClick={() => { setDropdownOpen(!dropdownOpen) }}>
+            {subCategory}
+            <svg className="w-2.5 h-2.5 ms-1 mt-0.5 me-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+              <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 4 4 4-4" />
+            </svg>
+          </button>
+          {dropdownOpen && <CategoryWriteDropdown setSubCategory={setSubCategory} setDropdownOpen={setDropdownOpen} />}
+          <div className="my-4 mx-2">
+            <input
+              className="p-2 w-full border border-gray-400 text-gray-900 bg-white focus:ring-2 focus:outline-none focus:ring-gray-300 rounded inline-flex items-center"
+              type="text"
+              value={title}
+              onChange={e => setTitle(e.target.value)}
+              placeholder="제목을 입력해 주세요."
+            />
+          </div>
+          <QuillEditor setContent={setContent} />
+          <div className="flex">
+
+          <button
+            className="mt-8 mb-4 ml-auto mr-2 p-1 w-20 rounded border border-gray-500 text-gray-900 bg-white focus:ring-2 focus:outline-none"
+            onClick={handleSubmit}
+            >
+            등록
+          </button>
+            </div>
+        </div>
       </div>
-      <PopularPosts posts={posts} />
+      <PopularPosts />
     </div>
   )
 }
