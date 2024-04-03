a3bbefa503dab84b6f0d8f678485a073c8ae845d
jk <yjkeong@gmail.com>
2024-04-04 00:17:07 +0900

    🔨 chore : 投稿一覧に該当する投稿がない場合の処理

--- src/components/Board/PostList.tsx
+++ src/components/Board/PostList.tsx
@@ -44,13 +44,13 @@ const PostList = ({ posts }: { posts: Post[] }) => {
   }
 
   // html 태그를 제외하고 순수 텍스트만 추출하는 함수
-  function htmlToText(content: string){
+  function htmlToText(content: string) {
     const reg = /<[^>]*>?/g; // html 태그 정규식
     return content.replace(reg, '');
   }
 
   // 글 내용이 82자 이상이면 잘라서 반환하는 함수
-  function cutText(content: string){
+  function cutText(content: string) {
     const maxLength = 82;
     if (content.length >= maxLength) {
       return content.substring(0, maxLength) + "...";
@@ -61,39 +61,46 @@ const PostList = ({ posts }: { posts: Post[] }) => {
 
   return (
     <div className="bg-gray-200 px-5 mb-5 rounded">
-      {posts.map((post) => (
-        <div key={post.id} className="py-4 flex justify-between">
-          <div className="w-full">
-            <div className="text-lg font-bold">
-              <Link to={`/board/post/${post.id}`}>{post.title}</Link>
-            </div>
-            <div className="text-gray-600 pt-2">
-              <Link to={`/board/post/${post.id}`}>{cutText(htmlToText(post.content))}</Link>
-            </div>
-            <div className="pt-4">
-              <span className="text-gray-400">by </span>
-              <span className="text-gray-600 pe-2">{post.user_id}</span>
-              <span className="text-gray-600 pe-2">•</span>
-              <span className="text-gray-600 pe-2">{timeFromNow(post.created_at)}</span>
-              {/* 실제 api와 연결해서 댓글 수 출력하는 코드 임시 */}
-              {/* {comments[post.id] && comments[post.id].length > 0 && ( // 댓글이 있는지 검사 -> 댓글이 1개 이상 있는지 검사
+      {posts.length === 0 ?
+        <div className="min-h-96">
+          글이 없습니다.
+        </div> : 
+        <>
+          {posts.map((post) => (
+            <div key={post.id} className="py-4 flex justify-between">
+              <div className="w-full">
+                <div className="text-lg font-bold">
+                  <Link to={`/board/post/${post.id}`}>{post.title}</Link>
+                </div>
+                <div className="text-gray-600 pt-2">
+                  <Link to={`/board/post/${post.id}`}>{cutText(htmlToText(post.content))}</Link>
+                </div>
+                <div className="pt-4">
+                  <span className="text-gray-400">by </span>
+                  <span className="text-gray-600 pe-2">{post.user_id}</span>
+                  <span className="text-gray-600 pe-2">•</span>
+                  <span className="text-gray-600 pe-2">{timeFromNow(post.created_at)}</span>
+                  {/* 실제 api와 연결해서 댓글 수 출력하는 코드 임시 */}
+                  {/* {comments[post.id] && comments[post.id].length > 0 && ( // 댓글이 있는지 검사 -> 댓글이 1개 이상 있는지 검사
               <>
               <span className="text-gray-400">•</span>
               <FaCommentDots className="inline-block pe-1 pb-1"/>
               <span className="text-gray-600 pe-2">{comments[post.id].length} comments</span>
               </>
               )} */}
-              <span className="text-gray-600 pe-2">•</span>
-              <BiSolidLike className="inline-block pe-1 pb-1" />
-              <span className="text-gray-600 pe-2">{post.likes}</span>
+                  <span className="text-gray-600 pe-2">•</span>
+                  <BiSolidLike className="inline-block pe-1 pb-1" />
+                  <span className="text-gray-600 pe-2">{post.likes}</span>
+                </div>
+              </div>
+              <div className="w-20">
+                {/* 사용자 프로필 이미지 차후 구현 */}
+                <img src="/src/assets/profile.png" alt="" />
+              </div>
             </div>
-          </div>
-          <div className="w-20">
-            {/* 사용자 프로필 이미지 차후 구현 */}
-            <img src="/src/assets/profile.png" alt="" />
-          </div>
-        </div>
-      ))}
+          ))}
+        </>
+      }
     </div>
   );
 };
