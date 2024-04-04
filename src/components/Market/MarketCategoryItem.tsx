2265c1f799e64739e3734c21d09dd512f7c3785c
baeseokmin <jessica5626@naver.com>
2024-04-04 13:26:15 +0900

    💄 style : マーケットページと決済ページのデザイン修正

--- src/components/Market/MarketCategoryItem.tsx
+++ src/components/Market/MarketCategoryItem.tsx
@@ -8,19 +8,19 @@ interface MarketCategoryItemProps {
 
 const MarketCategoryItem: React.FC<MarketCategoryItemProps> = ({ title, count, image }) => {
   return (
-    <div className="bg-white rounded hover:bg-gray-200 overflow-hidden shadow-md md:w-80 h-20 flex items-center">
-      <div className="p-4 flex items-center w-full">
-        <div className="w-14 h-14 rounded-full overflow-hidden">
+    <div className="bg-white rounded hover:bg-gray-200 overflow-hidden shadow-md md:w-64 h-16 flex items-center">
+      <div className="p-2 flex items-center w-full">
+        <div className="w-12 h-12 rounded-full overflow-hidden">
           <img
             src={image}
             alt="category"
             className="w-full h-full object-cover"
           />
         </div>
-        <div className="px-4 py-2 flex-grow">
+        <div className="px-2 py-1 flex-grow">
           <div className="flex items-center justify-between">
-            <h3 className="text-lg text-gray-700 font-bold">{title}</h3>
-            <div className="bg-gray-300 font-bold px-2">{count}</div>
+            <h3 className="text-gray-700 font-bold">{title}</h3>
+            <div className="bg-gray-300 font-bold px-1">{count}</div>
           </div>
         </div>
       </div>
