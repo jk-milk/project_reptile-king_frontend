05680062da10f84982ddf305aca04e83cf1b5c58
jk <yjkeong@gmail.com>
2024-04-09 12:36:27 +0900

    ✨ feat : ページ移動時にスクロールを一番上に移動させる機能を追加

---
+++ src/components/common/ScrollToTop.ts
@@ -0,0 +1,13 @@
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
+
+// 페이지 이동 시 스크롤 위치 조정
+export default function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+}
