9aa75c0deee72c7d7858f29c2c7dd9dcab233580
jk <yjkeong@gmail.com>
2024-04-15 17:22:00 +0900

    🔨 chore : context API関連コードのフォルダ構造の再構築によるパス変更

--- src/Pages/SignIn.tsx
+++ src/Pages/SignIn.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import { useAuth } from '../components/auth/useAuth';
+import { useAuth } from '../hooks/useAuth';
 import { API } from '../config';
 import { apiWithoutAuth } from '../components/common/axios';
 
