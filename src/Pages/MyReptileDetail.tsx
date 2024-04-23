2ffc35cabe0547ae8a0baff5a2918cd24f9ea64a
jk <yjkeong@gmail.com>
2024-04-24 06:20:28 +0900

    🔨 chore : 爬虫類詳細ページAPI接続

--- src/Pages/MyReptileDetail.tsx
+++ src/Pages/MyReptileDetail.tsx
@@ -1,43 +1,74 @@
-import { reptile } from './MyCage';
-import { Link } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { apiWithAuth } from '../components/common/axios';
+import { API } from '../config';
+import { Link, useLocation, useNavigate, useParams } from 'react-router-dom';
+import { Reptile } from '../types/Cage';
+import { calculateAge } from '../utils/CalculateAge';
 
 function MyReptileDetail() {
-  const firstReptile = reptile[0];
+  const navigate = useNavigate();
+  const { id } = useParams();
+  const location = useLocation();
+  const reptileSerialCode = location.state.reptileSerialCode;
 
-  const handleEditCage = () => {
-    const confirmSubmit = window.confirm('파충류 정보를 수정하시겠습니까?');
-    if (confirmSubmit) {
-      window.location.href = '/my-cage/reptile/edit';
-    }
-  };
+  const [reptile, setReptile] = useState<Reptile>();
+  console.log(id);
   
-  const handleSaveMemo = () => {
-    alert(`메모가 저장되었습니다.`);
-  };
+
+  // 파충류 상세 데이터 가져오기
+  useEffect(() => {
+    const fetchReptile = async () => {
+      const response = await apiWithAuth.get(API + "reptiles/" + reptileSerialCode);
+      console.log(response);
+      setReptile(response.data.reptile);
+    };
+    fetchReptile();
+  }, [reptileSerialCode]);
+
+  const handleEdit = () => {
+    navigate(`/my-cage/reptile/edit/${id}`, {
+      state: reptile
+    });
+  }
+
+  const handleDelete = async () => {
+    const check = confirm("정말 삭제하시겠습니까?");
+    if (check) {
+      console.log(id);
+      const response = await apiWithAuth.delete(API + "reptiles/" + id);
+      console.log(response);
+      alert("삭제되었습니다.")
+      navigate("/my-cage");
+    }
+  }
 
   return (
     <div className="pt-10 pb-10 mx-auto max-w-screen-lg">
       <div className="bg-white rounded-lg shadow-md px-5 py-4">
-        <div className="font-bold text-3xl mb-3">{firstReptile.repName}</div>
+        <div className="font-bold text-3xl mb-3">{reptile?.name}</div>
         <div className="flex items-center mb-20">
-          {/* Cage Image */}
-          {firstReptile.repImage && (
+          {reptile?.img_urls && (
             <div className="w-1/2 pr-6">
-              <img src={firstReptile.repImage} alt={firstReptile.repImage} className="w-full h-auto rounded-lg" />
+              <img src={reptile?.img_urls[0]} alt="파충류 이미지" className="w-full h-auto rounded-lg" />
             </div>
           )}
 
-          {/* Reptile Info */}
           <div className="w-1/2">
-            <div className="flex items-center mb-3"> {/* Modified */}
+            <div className="flex items-center mb-3">
               <div className="font-bold text-3xl">파충류 정보</div>
               
               <div className="ml-auto">
                 <button
-                  onClick={handleEditCage}
+                  onClick={handleEdit}
                   className="bg-blue-600 hover:bg-blue-400 text-white font-semibold py-1 px-3 rounded transition duration-300"
                 >
-                  파충류 수정하기
+                  수정
+                </button>
+                <button
+                  onClick={handleDelete}
+                  className="bg-red-600 hover:bg-red-400 text-white font-semibold ml-2 py-1 px-3 rounded transition duration-300"
+                >
+                  삭제
                 </button>
               </div>
             </div>
@@ -46,15 +77,15 @@ function MyReptileDetail() {
               <tbody>
                 <tr>
                   <td className="font-semibold text-xl">이름</td>
-                  <td className="text-lg">{firstReptile.repName}</td>
+                  <td className="text-lg">{reptile?.name}</td>
                 </tr>
                 <tr>
                   <td className="font-semibold text-xl">종</td>
-                  <td className="text-lg">{firstReptile.repType}</td>
+                  <td className="text-lg">{reptile?.species}</td>
                 </tr>
                 <tr>
                   <td className="font-semibold text-xl">나이</td>
-                  <td className="text-lg">{firstReptile.repAge}</td>
+                  <td className="text-lg">{calculateAge(reptile?.birth)}</td>
                 </tr>
               </tbody>
             </table>
@@ -70,12 +101,10 @@ function MyReptileDetail() {
         <div className="font-bold text-3xl mt-20">메모</div>
         <hr className="border-t border-gray-400 mt-3 mb-3" />
         <textarea
-          className="w-full h-40 border border-gray-300 rounded-md p-2 focus:outline-none"
-          placeholder="메모를 입력해 주세요..."
+          className="w-full h-40 border border-gray-300 rounded-md p-2 focus:outline-none cursor-default"
+          value={reptile?.memo || ""} // memo가 falsy 값일 경우 빈 문자열로 대체
+          readOnly
         ></textarea>
-        <div className="flex justify-end">
-          <button onClick={handleSaveMemo} className="bg-green-600 hover:bg-green-400 text-white font-semibold py-1 px-4 rounded transition duration-300 mt-1">저장</button>
-        </div>
         <div className="flex justify-center">
           <Link to="/my-cage" className="bg-blue-500 hover:bg-blue-400 text-white font-semibold py-2 px-4 rounded mt-16 transition duration-300">목록으로 돌아가기</Link>
         </div>
