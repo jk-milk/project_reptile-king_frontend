254fa54e95df3ad89c4d1cff2e479f2db3dff2b2
jk <yjkeong@gmail.com>
2024-04-24 19:33:24 +0900

    ✨ feat : ケージと爬虫類リストページで、飼育場データに現在の温湿度データを合わせた状態管理とそのためのタイプを追加

--- src/types/Cage.ts
+++ src/types/Cage.ts
@@ -13,6 +13,23 @@ export interface Cage {
   expired_at: string | null;
 }
 
+export interface CurCage {
+  id: number;
+  cur_temp: number | null;
+  cur_hum: number | null;
+  name: string;
+  serial_code: string;
+  user_id: number;
+  reptile_serial_code: string | null;
+  memo: string;
+  set_temp: number;
+  set_hum: number;
+  img_urls: string[];
+  created_at: string;
+  updated_at: string;
+  expired_at: string | null;
+}
+
 export interface Reptile {
   id: number;
   name: string;
