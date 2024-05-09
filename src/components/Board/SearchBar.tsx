a484470a2bc3662474f8828709f5ccfd61ee9fbd
jk <yjkeong@gmail.com>
2024-05-09 20:46:00 +0900

    ✨ feat : 掲示板用検索窓コンポーネントの実装

---
+++ src/components/Board/SearchBar.tsx
@@ -0,0 +1,34 @@
+import { ChangeEvent, FormEvent, useState } from "react";
+
+// 게시판 검색창
+function SearchBar({ handleSearchChange }: { handleSearchChange: (searchTerm: string) => void }) {
+  const [inputValue, setInputValue] = useState('');
+
+  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setInputValue(e.target.value);
+  };
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    handleSearchChange(inputValue);
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className="mt-5">
+      <input
+        type="text"
+        value={inputValue}
+        onChange={handleInputChange}
+        className="max-w-40 border-s border-y border-gray-300 rounded-s-md text-sm ml-2 px-4 py-2 leading-tight focus:outline-none focus:border-green-500"
+      />
+      <button
+        type="submit"
+        className="text-white bg-green-500 border-y border-e border-green-500 rounded-e-md leading-tight focus:outline-none focus:border-green-500 font-semibold text-sm px-2 py-2"
+      >
+        검색
+      </button>
+    </form>
+  );
+}
+
+export default SearchBar;
