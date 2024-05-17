408ff83574a8261d145112a69bab06ce4d8242aa
jk <yjkeong@gmail.com>
2024-05-17 09:28:25 +0900

    ✨ feat : 画像保存用のタイプを追加

--- src/types/Board.ts
+++ src/types/Board.ts
@@ -1,17 +1,17 @@
-import { Category } from "./Category";
-
 export interface Post {
   id: number;
   user_id: number;
+  nickname: string;
   title: string;
   content: string;
   category_id: number;
   category_name: string;
-  img_urls: { [key: string]: string };
+  img_urls: { [key: string]: string } | null;
   views: number;
   likes: number;
   created_at: string;
   updated_at: string;
+  comments: [];
 }
 
 // 서버에서 가져온 카테고리에서 Post 관련 부분만 사용하기 편하게 타입 변경
@@ -21,10 +21,16 @@ export interface PostCategory {
   division: string;
   parent_id: null;
   img_url: null;
-  subPosts: Category[];
+  subPosts: {
+    id: number;
+    name: string;
+    division: string;
+    parent_id: null;
+    img_url: null;
+  }[];
 }
 
-export interface Comment {
+export interface CommentsData {
   id: number;
   user_id: number;
   post_id: number;
@@ -40,4 +46,10 @@ export interface Comment {
 export interface SelectedCategory {
   id: number | null;
   name: string;
+}
+
+export interface ImageInfo {
+  blob: Blob;
+  imgUrl: string;
+  uniqueId: string; // 이미지 고유 ID
 }
