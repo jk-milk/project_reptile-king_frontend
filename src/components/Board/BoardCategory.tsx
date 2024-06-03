1c1139a91d0d661683fd1aebe18d23577bb405b9
jk <yjkeong@gmail.com>
2024-06-03 10:02:54 +0900

    💄 style : 背景色変更に伴う掲示板カテゴリーのデザイン修正

--- src/components/Board/BoardCategory.tsx
+++ src/components/Board/BoardCategory.tsx
@@ -6,14 +6,14 @@ interface BoardCategoryProps {
   onSelectCategory: (id: number) => void;
 }
 
-function BoardCategory({ categories, selectedCategory, onSelectCategory }: BoardCategoryProps) {  
+function BoardCategory({ categories, selectedCategory, onSelectCategory }: BoardCategoryProps) {   
   return (
     <div className="w-52 mr-20">
       <div className="mt-4">
         {categories.map((mainCategory) => (
           <div key={mainCategory.id} className="w-full p-2">
             <div className="border-b-2 pb-2">
-              <p className="text-lg text-white ps-2">
+              <p className="text-lg ps-2">
                 {mainCategory.name}
               </p>
             </div>
@@ -23,8 +23,8 @@ function BoardCategory({ categories, selectedCategory, onSelectCategory }: Board
                   <button
                     className={
                       selectedCategory.id === subCategory.id
-                        ? "ps-2 py-0.5 text-white font-bold hover:underline"
-                        : "ps-2 py-0.5 text-white hover:font-bold hover:underline"
+                        ? "ps-2 py-0.5 font-bold hover:underline"
+                        : "ps-2 py-0.5 hover:font-bold hover:underline"
                     }
                     onClick={() => onSelectCategory(subCategory.id)}
                   >
