d90a31c2924e08a3fcefa80b4402b644c84c050c
jk <yjkeong@gmail.com>
2024-06-03 20:20:21 +0900

    🔨 chore : 掲示板の記事並べ替えボタンコードの簡素化

--- src/components/Board/FilterButtons.tsx
+++ src/components/Board/FilterButtons.tsx
@@ -1,25 +1,25 @@
+import { useState } from "react";
 import { RiFileList2Line } from "react-icons/ri";
 import { FaFire } from "react-icons/fa";
 
-type FilterButtonsProps = {
-  handleTabChange: (tab: string) => void;
+interface FilterButtonsProps {
+  handleSortChange: (sort: string) => void;
   sort: string;
-  setSort: (sort: string) => void;
-  dropdownOpen: boolean;
-  setDropdownOpen: (isOpen: boolean) => void;
 }
 
-const FilterButtons = ({ handleTabChange, sort, setSort, dropdownOpen, setDropdownOpen }: FilterButtonsProps) => {
+const FilterButtons = ({ handleSortChange, sort }: FilterButtonsProps) => {
+  const [dropdownOpen, setDropdownOpen] = useState(false);
+
   return (
     <div>
       <button className="text-gray-900 border border-gray-100 focus:outline-none bg-white hover:bg-gray-100 focus:ring-1 focus:ring-gray-300 font-semibold text-sm pl-2 pr-2 py-1.5"
-        onClick={() => handleTabChange('all')}
+        onClick={() => handleSortChange('')}
       >
         <RiFileList2Line className="inline-block pe-1 pb-1" />
         전체글
       </button>
       <button className="text-white border border-red-700 focus:outline-none bg-red-700 hover:bg-red-800 focus:ring-1 focus:ring-red-700 font-semibold text-sm pl-2 pr-2 py-1.5"
-        onClick={() => handleTabChange('popular')}
+        onClick={() => handleSortChange('popular')}
       >
         <FaFire className="inline-block pe-1 pb-1" />
         인기글
@@ -37,10 +37,10 @@ const FilterButtons = ({ handleTabChange, sort, setSort, dropdownOpen, setDropdo
         <div className="absolute z-10 bg-white divide-y divide-gray-100 rounded-lg shadow mt-1 ml-40 w-44">
           <ul className="py-2 text-sm text-gray-700">
             <li>
-              <button onClick={() => { setSort('최신순'); setDropdownOpen(false); }} className="block px-4 py-2 w-full text-left hover:bg-gray-100">최신순</button>
+              <button onClick={() => { setDropdownOpen(false); handleSortChange('latest'); }} className="block px-4 py-2 w-full text-left hover:bg-gray-100">latest</button>
             </li>
             <li>
-              <button onClick={() => { setSort('오래된 순'); setDropdownOpen(false); }} className="block px-4 py-2 w-full text-left hover:bg-gray-100">등록순</button>
+              <button onClick={() => { setDropdownOpen(false); handleSortChange('oldest'); }} className="block px-4 py-2 w-full text-left hover:bg-gray-100">oldest</button>
             </li>
             {/* 추후 '댓글순' 추가 가능 */}
           </ul>
