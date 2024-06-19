7cf67ba19751049a5ad93fda0cfc81c09fcac71e
jk <yjkeong@gmail.com>
2024-06-19 14:46:32 +0900

    🔨 chore : バックグラウンド通知のテストコードのコメント処理

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -26,11 +26,11 @@ const messaging = firebase.messaging();
 
 messaging.onBackgroundMessage((payload) => {
   console.log('[firebase-messaging-sw.js] Received background message ', payload);
-  const notificationTitle = 'Background Message Title';
-  const notificationOptions = {
-    body: 'Background Message body.',
-    icon: '/firebase-logo.png'
-  };
+  // const notificationTitle = 'Background Message Title';
+  // const notificationOptions = {
+  //   body: 'Background Message body.',
+  //   icon: '/firebase-logo.png'
+  // };
 
   self.registration.showNotification(notificationTitle, notificationOptions);
 });
