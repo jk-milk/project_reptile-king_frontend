5fe143380820246d9aa944ad3d9c26e41cd845c4
jk <yjkeong@gmail.com>
2024-07-03 09:48:45 +0900

    🔨 chore : 通知表記エラーの修正

--- src/Pages/Notifications.tsx
+++ src/Pages/Notifications.tsx
@@ -156,7 +156,7 @@ const NotificationsPage = () => {
                   <span className="text-gray-600 mb-2">{formatDate(notification.created_at)}</span>
                   <div className="mb-4">{notification.content}</div>
                 </div>
-                {notification.category === 'reptile_sales' && (
+                {notification.category === 'reptile_sales'&& notification.title !== '爬虫類の譲渡完了' && (
                   <div className="notification-actions">
                     <button
                       className="bg-green-500 hover:bg-green-600 text-white rounded-md px-4 py-2 mr-2"
