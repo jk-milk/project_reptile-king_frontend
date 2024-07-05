eee8c55b0f595e5b2cb7f46eec2cf681ab18471b
jk <yjkeong@gmail.com>
2024-07-05 16:55:54 +0900

    🔨 chore : ログインが必要な場合、通知ウィンドウを日本語に変更

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -7,7 +7,7 @@ const ProtectedRoute = () => {
 
   // 인증되지 않은 사용자는 로그인 페이지로 리다이렉트
   if (!isAuthenticated) {
-    alert("로그인해 주세요!");
+    alert("ログインしてください！");
     return <Navigate to="/login" replace />;
   }
 
